43cfa31055c30c5ed6192f18b0fad53d825f682e
Shai Sussman <shai.sussman@gmail.com>
2024-12-19 15:40:29 +0200

    Add Polygons data

--- app.py
+++ app.py
@@ -7,13 +7,21 @@ import pandas as pd
 pd.options.display.max_columns = 150
 # Load the data
 
+stats_data_gdf = gpd.read_file('data/stat_pop_simpl_votes_2022.geojson')
+stats_data_gdf.to_crs('EPSG:4326', inplace=True)
+stats_data = stats_data_gdf.__geo_interface__
+
 app = Dash()
 app.layout = html.Div([
     html.Div(
         [
             'hello World',
             dl.Map([
-                dl.TileLayer(url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png')],
+                dl.TileLayer(
+                    url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'),
+                dl.GeoJSON(id='stats_layer', data=stats_data,
+                           options=dict(style=dict(color='blue')))
+            ],
                 center=[32, 34],
                 zoom=7,
                 style={'height': '50vh'},
