0ba62dac39c0e31bf1a8756321a215e7fd0444b6
Shai Sussman <shai.sussman@gmail.com>
2024-12-19 23:20:19 +0200

    return get info back to method

--- app_helper.py
+++ app_helper.py
@@ -3,14 +3,6 @@ from dash_extensions.javascript import arrow_function, assign
 from dash import Dash, dcc, html, Input, Output
 
 
-def get_info(feature=None):
-    header = []
-    if not feature:
-        return header
-    return header + [html.B(feature["properties"]["Shem_Yishuv"]), html.B(" "), html.B(feature["properties"]["sta_22_names"]), html.Br(),
-                     html.Span(col_rename.get(feature["properties"]["max_label"]))]
-
-
 style_handle = assign("""function(feature, context){
     const {classes, colorscale, style, colorProp} = context.hideout;  // get props from hideout
     const value = feature.properties[colorProp];  // get value the determines the color
