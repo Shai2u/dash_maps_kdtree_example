f52e8945992383d6f0829c00a92f8703e8911fa5
Shai Sussman <shai.sussman@gmail.com>
2024-12-21 19:57:35 +0200

    preperation for KDtree rename prefix

--- app_helper.py
+++ app_helper.py
@@ -15,9 +15,21 @@ won_style_handle = assign("""function(feature, context){
 }""")
 
 
-won_style = {'color': 'white',  'fillOpacity': 0.9, 'weight': 1.5}
+kde_style_handle = assign("""function(feature, context){
+    const {classes, colorscale, style, colorProp} = context.hideout;  // get props from hideout
+    const value = feature.properties[colorProp];  // get value the determines the color
+    for (let i = 0; i < classes.length; ++i) {
+        if (value > classes[i]) {
+            style.fillColor = colorscale[i];  // set the fill color according to the class
+        }
+    }
+    return style;
+}""")
 
-won_hover_style = {'color': 'white',  'fillOpacity': 0.5, 'weight': 3}
+
+style = {'color': 'white',  'fillOpacity': 0.9, 'weight': 1.5}
+
+hover_style = {'color': 'white',  'fillOpacity': 0.5, 'weight': 3}
 
 map_analysis_radio_options = [
     {'label': 'Who Won', 'value': 'who_won'},
