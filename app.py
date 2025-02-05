e7271f5f82b08eed1e8603e4d5e2bfb530d7bf7c
Shai Sussman <shai.sussman@gmail.com>
2025-02-05 11:43:34 +0200

    Change font size

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def build_near_clsuter_bar_fig(gdf_sorted, kdtree_distance):
         yaxis_showgrid=False
     )
     fig.update_traces(texttemplate='%{y:.2f}', textposition='outside')
-    fig.update_layout(margin=dict(l=0, r=0, t=30, b=0), font=dict(size=18))
+    fig.update_layout(margin=dict(l=0, r=0, t=30, b=0), font=dict(size=14))
     return fig
 
 
@@ -141,7 +141,7 @@ def generate_barplot(feature=None):
         yaxis_showgrid=False
     )
     fig.update_traces(texttemplate='%{y:.1%}', textposition='outside')
-    fig.update_layout(margin=dict(l=0, r=0, t=0, b=0), title_y=0.9, title_x=0.95, font=dict(size=18))
+    fig.update_layout(margin=dict(l=0, r=0, t=0, b=0), title_y=0.9, title_x=0.95, font=dict(size=14))
     return fig
 
 
